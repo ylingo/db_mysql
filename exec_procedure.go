d47819a8664cd060e5fb2fc3e756f138f24f2002
ylingo <yanglin_cn@163.com>
2019-05-05 14:43:32 +0800

    update query page

--- exec_procedure.go
+++ exec_procedure.go
@@ -3,218 +3,223 @@ package db_mysql
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 func QueryByPage(dest interface{},
 	table, fields, where, join, orderby string,
 	pageSize, pageIndex int) (totalcount, pagecount, outPageIndex int, err error) {
-
-	//先暂时使用这个拙劣的处理方式，后面需要研究一个优雅的方式来处理。
-	return QueryWithFoundRows(dest, table, fields, where, join, orderby, pageSize, pageIndex)
-
-	// defer checkerr()
-
-	// params := []PROCEDURE_PARAM{}
-	// var totalcount_p int = 0
-	// var pagecount_p int = 0
-	// params = append(params, PROCEDURE_PARAM{Name: "_fields", Direct: "in", Value: fields})
-	// params = append(params, PROCEDURE_PARAM{Name: "_table", Direct: "in", Value: table})
-	// params = append(params, PROCEDURE_PARAM{Name: "_where", Direct: "in", Value: where})
-	// params = append(params, PROCEDURE_PARAM{Name: "_join", Direct: "in", Value: join})
-	// params = append(params, PROCEDURE_PARAM{Name: "_orderby", Direct: "in", Value: orderby})
-	// params = append(params, PROCEDURE_PARAM{Name: "_pageIndex", Direct: "in", Value: pageIndex})
-	// params = append(params, PROCEDURE_PARAM{Name: "_pageSize", Direct: "in", Value: pageSize})
-	// params = append(params, PROCEDURE_PARAM{Name: "_totalcount", Direct: "out", Value: 0, ParamPoint: &totalcount_p})
-	// params = append(params, PROCEDURE_PARAM{Name: "_pagecount", Direct: "out", Value: 0, ParamPoint: &pagecount_p})
-
-	// err = ExecProcedureQuery(dest, "sp_page", params...)
-	// if err != nil {
-	// 	return 0, 0, err
-	// }
-	// totalcount = totalcount_p
-	// pagecount = pagecount_p
-	// outPageIndex = pageIndex
-	// return totalcount, pagecount,outPageIndex, nil
-}
-
-type PROCEDURE_PARAM struct {
-	Name       string
-	Direct     string
-	Value      interface{}
-	ParamPoint interface{}
-}
-
-func ExecProcedureQuery(dest interface{}, sqlProcedure string, params ...PROCEDURE_PARAM) error {
-	defer checkerr()
-	//var inParams []interface{}
-	var outParamsPoint []interface{}
+	if pageIndex <= 0 {
+		pageIndex = 1
+	}
+	if pageSize < 0 {
+		pageSize = 100
+	}
 	var _sb bytes.Buffer
-	outParams := []PROCEDURE_PARAM{}
-
-	for i := range params {
-		if (strings.EqualFold(params[i].Direct, "inout") ||
-			strings.EqualFold(params[i].Direct, "in")) &&
-			params[i].Value != nil {
-			_sb.WriteString("set @")
-			_sb.WriteString(params[i].Name)
-			_sb.WriteString("=")
-			switch params[i].Value.(type) {
-			case int, int8, int16, int32, int64:
-				_sb.WriteString(fmt.Sprintf("%d", params[i].Value))
-			case float32, float64:
-				_sb.WriteString(fmt.Sprintf("%f", params[i].Value))
-			case string:
-				_sb.WriteString("\"")
-
-				//由于在执行存储过程的方式是先set变量值，再执行的CALL
-				//设置变量时需要将转义符替换成双数，这样在mysql中才不至于出错
-				_sb.WriteString(fmt.Sprintf("%s", strings.Replace(params[i].Value.(string), "\\", "\\\\", -1)))
-				_sb.WriteString("\"")
-			}
-			_sb.WriteString(";")
-		}
-	}
-	_sb.WriteString(" call ")
-	_sb.WriteString(sqlProcedure)
-	_sb.WriteString("(")
-	for i := range params {
-		if i != 0 {
-			_sb.WriteString(",")
-		}
-		if strings.EqualFold(params[i].Direct, "in") {
-			//_sb.WriteString("?")
-			//inParams = append(inParams, params[i].Value)
-			_sb.WriteString("@")
-			_sb.WriteString(params[i].Name)
-		} else if strings.EqualFold(params[i].Direct, "out") ||
-			strings.EqualFold(params[i].Direct, "inout") {
-			_sb.WriteString("@")
-			_sb.WriteString(params[i].Name)
-			outParamsPoint = append(outParamsPoint, params[i].ParamPoint)
-			outParams = append(outParams, params[i])
-		}
-
-	}
-	_sb.WriteString(");")
-	//fmt.Println(_sb.String())
-	//err := database.Select(dest, _sb.String(), inParams...)
-	err := database.Select(dest, _sb.String())
+	_sb.WriteString("call sp_page(")
+	_sb.WriteString(fmt.Sprintf("'%s',", fields))
+	_sb.WriteString(fmt.Sprintf("'%s',", table))
+	_sb.WriteString(fmt.Sprintf("'%s',", join))
+	_sb.WriteString(fmt.Sprintf("'%s',", where))
+	_sb.WriteString(fmt.Sprintf("'%s',", orderby))
+	_sb.WriteString(fmt.Sprintf("%d,", pageIndex))
+	_sb.WriteString(fmt.Sprintf("%d,", pageSize))
+	_sb.WriteString(fmt.Sprintf("@totalcount,@pagecount"))
+	_sb.WriteString(")")
+	fmt.Println(_sb.String())
+	err = database.Select(dest, _sb.String())
 	if err != nil {
-		errorinfo("ExecProcedureQuery, %s ,err:%s", _sb.String(), err)
-		return err
+		return 0, 0, 0, err
 	}
-
-	var _sb1 bytes.Buffer
-	if len(outParams) > 0 {
-		_sb1.WriteString("select ")
-		for i := range outParams {
-			if i != 0 {
-				_sb1.WriteString(",")
-			}
-			_sb1.WriteString("@")
-			_sb1.WriteString(outParams[i].Name)
-			_sb1.WriteString(" as ")
-			_sb1.WriteString(outParams[i].Name)
+	rows := database.QueryRow("SELECT @totalcount as totalcount,@pagecount as pagecount")
+	if err = rows.Scan(&totalcount, &pagecount); err == nil {
+		outPageIndex = pageIndex
+		if pageIndex > pagecount {
+			pageIndex = pagecount
+			return QueryByPage(dest, table, fields, where, join, orderby, pageSize, pageIndex)
 		}
-		_sb1.WriteString(";") //try append semicolon
-		rows1 := database.QueryRow(_sb1.String())
-		if err = rows1.Scan(outParamsPoint...); err != nil {
-			errorinfo("ExecProcedureQuery,%s %s,err:%s", sqlProcedure, _sb1.String(), err)
-			return err
-		}
+		return totalcount, pagecount, outPageIndex, nil
+	} else {
+		return 0, 0, 0, err
 	}
-	loginfo("ExecProcedureQuery, %s %s", _sb.String(), _sb1.String())
-
-	return nil
 }
 
-func ExecProcedureNoQuery(sqlProcedure string, params ...PROCEDURE_PARAM) error {
-	//	执行存储过程时，如果存在inout,则必须是先设置参数形式，不能用？替换符
-	//	即 set @param1=1;set @param2='2';set @param4=4;set @param5=5; call p1(@param1,@param2,@param3,@param4,@param5);
-	//	   select @param3,@param4,@param5
-	//	以上格式
-	//	不允许 set @param1; call p1(?,?,@param3)
-	//	注：只要有 inout类型，必须先对in，inout类型设置变量，然后call
-	// 由于涉及到多个SQL 语句同时执行，需要在go-sql-driver/mysql/packets.go中writeAuthPacket函数增加clientMultiStatements标志，大约在244行
-	//var inParams []interface{}
-	var outParamsPoint []interface{}
-	var _sb bytes.Buffer
-	outParams := []PROCEDURE_PARAM{}
+// type PROCEDURE_PARAM struct {
+// 	Name       string
+// 	Direct     string
+// 	Value      interface{}
+// 	ParamPoint interface{}
+// }
 
-	for i := range params {
-		if (strings.EqualFold(params[i].Direct, "inout") ||
-			strings.EqualFold(params[i].Direct, "in")) &&
-			params[i].Value != nil {
-			_sb.WriteString("set @")
-			_sb.WriteString(params[i].Name)
-			_sb.WriteString("=")
-			switch params[i].Value.(type) {
-			case int, int8, int16, int32, int64:
-				_sb.WriteString(fmt.Sprintf("%d", params[i].Value))
-			case float32, float64:
-				_sb.WriteString(fmt.Sprintf("%f", params[i].Value))
-			case string:
-				_sb.WriteString("\"")
-				//由于在执行存储过程的方式是先set变量值，再执行的CALL
-				//设置变量时需要将转义符替换成双数，这样在mysql中才不至于出错
-				_sb.WriteString(fmt.Sprintf("%s", strings.Replace(params[i].Value.(string), "\\", "\\\\", -1)))
-				_sb.WriteString("\"")
-			}
-			_sb.WriteString("; ")
-		}
-	}
-	_sb.WriteString(" call ")
-	_sb.WriteString(sqlProcedure)
-	_sb.WriteString("(")
-	for i := range params {
-		if i != 0 {
-			_sb.WriteString(",")
-		}
-		if strings.EqualFold(params[i].Direct, "in") {
-			//_sb.WriteString("?")
-			//inParams = append(inParams, params[i].Value)
-			_sb.WriteString("@")
-			_sb.WriteString(params[i].Name)
-		} else if strings.EqualFold(params[i].Direct, "out") ||
-			strings.EqualFold(params[i].Direct, "inout") {
-			_sb.WriteString("@")
-			_sb.WriteString(params[i].Name)
-			outParamsPoint = append(outParamsPoint, params[i].ParamPoint)
-			outParams = append(outParams, params[i])
-		}
-	}
-	_sb.WriteString(");")
+// func ExecProcedureQuery(dest interface{}, sqlProcedure string, params ...PROCEDURE_PARAM) error {
+// 	defer checkerr()
+// 	//var inParams []interface{}
+// 	var outParamsPoint []interface{}
+// 	var _sb bytes.Buffer
+// 	outParams := []PROCEDURE_PARAM{}
 
-	//fmt.Println(_sb.String())
-	//for i := range inParams {
-	//	fmt.Println(inParams[i])
-	//}
-	//_, err := database.Exec(_sb.String(), inParams...)
-	_, err := database.Exec(_sb.String())
-	if err != nil {
-		errorinfo("ExecProcedureNoQuery, %s ,err:%s", _sb.String(), err)
-		return err
-	}
+// 	for i := range params {
+// 		if (strings.EqualFold(params[i].Direct, "inout") ||
+// 			strings.EqualFold(params[i].Direct, "in")) &&
+// 			params[i].Value != nil {
+// 			_sb.WriteString("set @")
+// 			_sb.WriteString(params[i].Name)
+// 			_sb.WriteString("=")
+// 			switch params[i].Value.(type) {
+// 			case int, int8, int16, int32, int64:
+// 				_sb.WriteString(fmt.Sprintf("%d", params[i].Value))
+// 			case float32, float64:
+// 				_sb.WriteString(fmt.Sprintf("%f", params[i].Value))
+// 			case string:
+// 				_sb.WriteString("\"")
 
-	var _sb1 bytes.Buffer
-	if len(outParams) > 0 {
-		_sb1.WriteString("select ")
-		for i := range outParams {
-			if i != 0 {
-				_sb1.WriteString(",")
-			}
-			_sb1.WriteString("@")
-			_sb1.WriteString(outParams[i].Name)
-			_sb1.WriteString(" as ")
-			_sb1.WriteString(outParams[i].Name)
-		}
-		rows1 := database.QueryRow(_sb1.String())
-		if err = rows1.Scan(outParamsPoint...); err != nil {
-			errorinfo("ExecProcedureNoQuery,%s  %s,err:%s", sqlProcedure, _sb1.String(), err)
-			return err
-		}
-	}
-	loginfo("ExecProcedureNoQuery, %s  %s", _sb.String(), _sb1.String())
-	return nil
-}
+// 				//由于在执行存储过程的方式是先set变量值，再执行的CALL
+// 				//设置变量时需要将转义符替换成双数，这样在mysql中才不至于出错
+// 				_sb.WriteString(fmt.Sprintf("%s", strings.Replace(params[i].Value.(string), "\\", "\\\\", -1)))
+// 				_sb.WriteString("\"")
+// 			}
+// 			_sb.WriteString(";")
+// 		}
+// 	}
+// 	_sb.WriteString(" call ")
+// 	_sb.WriteString(sqlProcedure)
+// 	_sb.WriteString("(")
+// 	for i := range params {
+// 		if i != 0 {
+// 			_sb.WriteString(",")
+// 		}
+// 		if strings.EqualFold(params[i].Direct, "in") {
+// 			//_sb.WriteString("?")
+// 			//inParams = append(inParams, params[i].Value)
+// 			_sb.WriteString("@")
+// 			_sb.WriteString(params[i].Name)
+// 		} else if strings.EqualFold(params[i].Direct, "out") ||
+// 			strings.EqualFold(params[i].Direct, "inout") {
+// 			_sb.WriteString("@")
+// 			_sb.WriteString(params[i].Name)
+// 			outParamsPoint = append(outParamsPoint, params[i].ParamPoint)
+// 			outParams = append(outParams, params[i])
+// 		}
+
+// 	}
+// 	_sb.WriteString(");")
+// 	//fmt.Println(_sb.String())
+// 	//err := database.Select(dest, _sb.String(), inParams...)
+// 	err := database.Select(dest, _sb.String())
+// 	if err != nil {
+// 		errorinfo("ExecProcedureQuery, %s ,err:%s", _sb.String(), err)
+// 		return err
+// 	}
+
+// 	var _sb1 bytes.Buffer
+// 	if len(outParams) > 0 {
+// 		_sb1.WriteString("select ")
+// 		for i := range outParams {
+// 			if i != 0 {
+// 				_sb1.WriteString(",")
+// 			}
+// 			_sb1.WriteString("@")
+// 			_sb1.WriteString(outParams[i].Name)
+// 			_sb1.WriteString(" as ")
+// 			_sb1.WriteString(outParams[i].Name)
+// 		}
+// 		_sb1.WriteString(";") //try append semicolon
+// 		rows1 := database.QueryRow(_sb1.String())
+// 		if err = rows1.Scan(outParamsPoint...); err != nil {
+// 			errorinfo("ExecProcedureQuery,%s %s,err:%s", sqlProcedure, _sb1.String(), err)
+// 			return err
+// 		}
+// 	}
+// 	loginfo("ExecProcedureQuery, %s %s", _sb.String(), _sb1.String())
+
+// 	return nil
+// }
+
+// func ExecProcedureNoQuery(sqlProcedure string, params ...PROCEDURE_PARAM) error {
+// 	//	执行存储过程时，如果存在inout,则必须是先设置参数形式，不能用？替换符
+// 	//	即 set @param1=1;set @param2='2';set @param4=4;set @param5=5; call p1(@param1,@param2,@param3,@param4,@param5);
+// 	//	   select @param3,@param4,@param5
+// 	//	以上格式
+// 	//	不允许 set @param1; call p1(?,?,@param3)
+// 	//	注：只要有 inout类型，必须先对in，inout类型设置变量，然后call
+// 	// 由于涉及到多个SQL 语句同时执行，需要在go-sql-driver/mysql/packets.go中writeAuthPacket函数增加clientMultiStatements标志，大约在244行
+// 	//var inParams []interface{}
+// 	var outParamsPoint []interface{}
+// 	var _sb bytes.Buffer
+// 	outParams := []PROCEDURE_PARAM{}
+
+// 	for i := range params {
+// 		if (strings.EqualFold(params[i].Direct, "inout") ||
+// 			strings.EqualFold(params[i].Direct, "in")) &&
+// 			params[i].Value != nil {
+// 			_sb.WriteString("set @")
+// 			_sb.WriteString(params[i].Name)
+// 			_sb.WriteString("=")
+// 			switch params[i].Value.(type) {
+// 			case int, int8, int16, int32, int64:
+// 				_sb.WriteString(fmt.Sprintf("%d", params[i].Value))
+// 			case float32, float64:
+// 				_sb.WriteString(fmt.Sprintf("%f", params[i].Value))
+// 			case string:
+// 				_sb.WriteString("\"")
+// 				//由于在执行存储过程的方式是先set变量值，再执行的CALL
+// 				//设置变量时需要将转义符替换成双数，这样在mysql中才不至于出错
+// 				_sb.WriteString(fmt.Sprintf("%s", strings.Replace(params[i].Value.(string), "\\", "\\\\", -1)))
+// 				_sb.WriteString("\"")
+// 			}
+// 			_sb.WriteString("; ")
+// 		}
+// 	}
+// 	_sb.WriteString(" call ")
+// 	_sb.WriteString(sqlProcedure)
+// 	_sb.WriteString("(")
+// 	for i := range params {
+// 		if i != 0 {
+// 			_sb.WriteString(",")
+// 		}
+// 		if strings.EqualFold(params[i].Direct, "in") {
+// 			//_sb.WriteString("?")
+// 			//inParams = append(inParams, params[i].Value)
+// 			_sb.WriteString("@")
+// 			_sb.WriteString(params[i].Name)
+// 		} else if strings.EqualFold(params[i].Direct, "out") ||
+// 			strings.EqualFold(params[i].Direct, "inout") {
+// 			_sb.WriteString("@")
+// 			_sb.WriteString(params[i].Name)
+// 			outParamsPoint = append(outParamsPoint, params[i].ParamPoint)
+// 			outParams = append(outParams, params[i])
+// 		}
+// 	}
+// 	_sb.WriteString(");")
+
+// 	//fmt.Println(_sb.String())
+// 	//for i := range inParams {
+// 	//	fmt.Println(inParams[i])
+// 	//}
+// 	//_, err := database.Exec(_sb.String(), inParams...)
+// 	_, err := database.Exec(_sb.String())
+// 	if err != nil {
+// 		errorinfo("ExecProcedureNoQuery, %s ,err:%s", _sb.String(), err)
+// 		return err
+// 	}
+
+// 	var _sb1 bytes.Buffer
+// 	if len(outParams) > 0 {
+// 		_sb1.WriteString("select ")
+// 		for i := range outParams {
+// 			if i != 0 {
+// 				_sb1.WriteString(",")
+// 			}
+// 			_sb1.WriteString("@")
+// 			_sb1.WriteString(outParams[i].Name)
+// 			_sb1.WriteString(" as ")
+// 			_sb1.WriteString(outParams[i].Name)
+// 		}
+// 		rows1 := database.QueryRow(_sb1.String())
+// 		if err = rows1.Scan(outParamsPoint...); err != nil {
+// 			errorinfo("ExecProcedureNoQuery,%s  %s,err:%s", sqlProcedure, _sb1.String(), err)
+// 			return err
+// 		}
+// 	}
+// 	loginfo("ExecProcedureNoQuery, %s  %s", _sb.String(), _sb1.String())
+// 	return nil
+// }
--- mysql_test.go
+++ mysql_test.go
@@ -6,12 +6,12 @@ import (
 
 func Init() {
 	InitDb("mysql",
-		"root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true&timeout=20s&loc=Local&clientFoundRows=true",
+		"root:adonis2008@tcp(db.dulala.co:3306)/ttttt?charset=utf8&parseTime=true&timeout=20s&loc=Local&clientFoundRows=true",
 		100,
 		20, true, true)
 }
 
-func Test_ExecNoQuery(t *testing.T) {
+func _Test_ExecNoQuery(t *testing.T) {
 	Init()
 	//	p := []Product{}
 	//	ExecQuery(&p, "select Id,Product_id,Product_name from tbl2 limit 1,100")
@@ -20,139 +20,69 @@ func Test_ExecNoQuery(t *testing.T) {
 	//	}
 
 	//err := ExecNoQuery("insert into tbl2(product_id,product_name) values('111','2222')")
-	err := ExecNoQuery("insert into tbl2(product_id,product_name) values(?,?)", "abc", "sdfdsfdsf")
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log("执行成功")
-	}
-
-}
-
-func Test_ExecInsertGetLastId(t *testing.T) {
-	Init()
-
-	lastId, err := ExecInsertGetLastId("insert into tbl2(product_id,product_name) values('111','2222')")
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log("执行成功,ID:", lastId)
-	}
-}
-
-type Product struct {
-	Id           int    `db:"id"`
-	Product_id   string `db:"product_id"`
-	Product_name string `db:"product_name"`
-}
-
-func Test_ExecQuery(t *testing.T) {
-	Init()
-
-	p := []Product{}
-	//p := make([]Product, 0)
-	err := ExecQuery(&p, "select id,product_id,product_name from tbl2 limit 1,100")
-	if err != nil {
-		t.Error(err)
-	} else {
-		for i := range p {
-			t.Log(p[i])
+	//strsql := "insert into tbl1(c2,c3,c4) values('1','11111','sssssss');"
+	strsql := "insert into tbl2(c22,c33,c44) values('2','99999','hhhhhhhh');"
+	// strsql += "insert into tbl1(c2,c3,c4) values('3','33333','sssssss');"
+	// strsql += "insert into tbl1(c2,c3,c4) values('4','44444','sssssss');"
+	// strsql += "insert into tbl1(c2,c3,c4) values('5','55555','sssssss');"
+	for i := 0; i < 100; i++ {
+		err := ExecNoQuery(strsql)
+		if err != nil {
+			t.Error(err)
+		} else {
+			t.Log("执行成功")
 		}
 	}
 }
+
+// func Test_ExecInsertGetLastId(t *testing.T) {
+// 	Init()
+
+// 	lastId, err := ExecInsertGetLastId("insert into tbl2(product_id,product_name) values('111','2222')")
+// 	if err != nil {
+// 		t.Error(err)
+// 	} else {
+// 		t.Log("执行成功,ID:", lastId)
+// 	}
+// }
+
+type Product struct {
+	C1  int    `db:"c1"`
+	C2  string `db:"c2"`
+	C3  string `db:"c3"`
+	C4  string `db:"c4"`
+	C33 string `db:"c33"`
+	C44 string `db:"c44"`
+}
+
+// func Test_ExecQuery(t *testing.T) {
+// 	Init()
+
+// 	p := []Product{}
+// 	//p := make([]Product, 0)
+// 	err := ExecQuery(&p, "select id,product_id,product_name from tbl2 limit 1,100")
+// 	if err != nil {
+// 		t.Error(err)
+// 	} else {
+// 		for i := range p {
+// 			t.Log(p[i])
+// 		}
+// 	}
+// }
 
 func TestQueryByPage(t *testing.T) {
 	Init()
 	dest := []Product{}
-	totalcount, totalpage, err := QueryByPage(&dest, "tbl2", "id,product_id,product_name", "", "", "", 100, 1)
+	totalcount, totalpage, outpageindex, err := QueryByPage(&dest, "tbl1 as t", "t.c1,t.c2,t.c3,t.c4,t2.c33,t2.c44", "",
+		"left join tbl2 as t2 on t.c1=t2.c11",
+		"", 100, 1)
 	if err != nil {
 		t.Error("totalcount", totalcount, "totalpage:", totalpage, "err:", err)
 	} else {
-		t.Log("totalcount", totalcount, "totalpage:", totalpage)
+		t.Log("totalcount", totalcount, ",totalpage:", totalpage, "outpageindex:", outpageindex)
+		t.Log(len(dest))
 		for i := range dest {
 			t.Log(dest[i])
 		}
 	}
 }
-
-func Test_ProcedureExecQuery(t *testing.T) {
-	Init()
-	dest := []Product{}
-
-	params := []PROCEDURE_PARAM{}
-	var totalcount int = 0
-	var pagecount int = 0
-	params = append(params, PROCEDURE_PARAM{Name: "fields", Direct: "in", Value: "id,product_id,product_name"})
-	params = append(params, PROCEDURE_PARAM{Name: "table", Direct: "in", Value: "tbl2"})
-	params = append(params, PROCEDURE_PARAM{Name: "where", Direct: "in", Value: ""})
-	params = append(params, PROCEDURE_PARAM{Name: "orderby", Direct: "in", Value: ""})
-	params = append(params, PROCEDURE_PARAM{Name: "pageIndex", Direct: "in", Value: 1})
-	params = append(params, PROCEDURE_PARAM{Name: "pageSize", Direct: "in", Value: 100})
-	params = append(params, PROCEDURE_PARAM{Name: "totalcount", Direct: "out", Value: "", ParamPoint: &totalcount})
-	params = append(params, PROCEDURE_PARAM{Name: "pagecount", Direct: "out", Value: "", ParamPoint: &pagecount})
-
-	err := ExecProcedureQuery(&dest, "sp_page", params...)
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log("totalcount", totalcount, "pagecount:", pagecount)
-		for i := range dest {
-			t.Log(dest[i])
-		}
-	}
-}
-
-func Test_ProcedureExecNoQuery(t *testing.T) {
-	Init()
-	var param3 int
-	var param4 int
-	var param5 string
-	params := []PROCEDURE_PARAM{}
-	params = append(params, PROCEDURE_PARAM{Name: "param1", Direct: "in", Value: 1})
-	params = append(params, PROCEDURE_PARAM{Name: "param2", Direct: "in", Value: "2"})
-	params = append(params, PROCEDURE_PARAM{Name: "param3", Direct: "out", Value: 0, ParamPoint: &param3})
-	params = append(params, PROCEDURE_PARAM{Name: "param4", Direct: "inout", Value: 4, ParamPoint: &param4})
-	params = append(params, PROCEDURE_PARAM{Name: "param5", Direct: "inout", Value: "5", ParamPoint: &param5})
-
-	err := ExecProcedureNoQuery("p1", params...)
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log("p3:", param3, "p4:", param4, "p5:", param5)
-	}
-
-}
-
-func Test_ProcedureExecNoQuery1(t *testing.T) {
-	Init()
-	var param3 int
-	var param4 int
-	params := []PROCEDURE_PARAM{}
-	params = append(params, PROCEDURE_PARAM{Name: "param1", Direct: "in", Value: 1})
-	params = append(params, PROCEDURE_PARAM{Name: "param2", Direct: "in", Value: "2"})
-	params = append(params, PROCEDURE_PARAM{Name: "param3", Direct: "out", Value: 0, ParamPoint: &param3})
-	params = append(params, PROCEDURE_PARAM{Name: "param4", Direct: "out", Value: 4, ParamPoint: &param4})
-
-	err := ExecProcedureNoQuery("p2", params...)
-	if err != nil {
-		t.Error(err)
-	} else {
-		t.Log("p3:", param3, "p4:", param4)
-	}
-
-}
-
-//func Test_Tx(t *testing.T) {
-//	var strSqls []string
-//	strSqls = append(strSqls, "insert into tbl2(product_id,product_name) values('1','11111')")
-//	strSqls = append(strSqls, "insert into tbl2(product_id,product_name) values('2','22222')")
-//	strSqls = append(strSqls, "insert into tbl2(product_id,product_name) values('3','33333')")
-//	strSqls = append(strSqls, "insert into tbl2(product_id,product_name) values('4','44444')")
-//	strSqls = append(strSqls, "insert into tbl2(product_id,product_name) values('5','55555')")
-//	err := ExecNoQueryTx(strSqls)
-//	if err != nil {
-//		t.Error(err)
-//	} else {
-//		t.Log("搞定")
-//	}
-//}
